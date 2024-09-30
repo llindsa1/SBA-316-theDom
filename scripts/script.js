4e9e4134cb2ed4ba0acbd7e9814539afe96bd275
llindsa1 <lindsayuchiha@icloud.com>
2024-09-30 09:41:05 -0400

    9th

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,34 @@ const display= document.getElementById('display');
 const numButtons= document.querySelectorAll('.num-btn');
 const opButtons= document.querySelectorAll('op-btn');
 
+//Variables
+let currentValue= '';
+let operator= '';
+let previousValue = '';
+
+function updateDisplay(value) {
+    display.value = value;
+}
+
+//Add Event Listeners
+numButtons.forEach(button => {
+    button.addEventListener('click' ,
+        function () {
+            currentValue+=
+            button.getAttribute('data-value');
+            updateDisplay(currentValue);
+        }
+    );
+});
+
+opButtons.forEach(button => {button.addEventListener 
+    ('click' ,
+        function ()  {
+            if (currentValue === '')
+                return;
+            operator = button.getAttribute('data-value');
+            previousValue = currentValue;
+            currentValue = '';
+        }
+    );
+});
