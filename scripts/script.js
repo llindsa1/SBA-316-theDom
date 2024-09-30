d006e227601d741395aced63096b6767861212df
llindsa1 <lindsayuchiha@icloud.com>
2024-09-30 09:22:55 -0400

    8th

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+//Cache the display element using getElementById
+const display= document.getElementById('display');
+
+//Cache at least one element using querySelector or querySelectorAll
+const numButtons= document.querySelectorAll('.num-btn');
+const opButtons= document.querySelectorAll('op-btn');
+
